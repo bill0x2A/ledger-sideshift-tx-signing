efea2145abc9ac82faf03d61ae6ec30952514568
bill0x2a <8884279+bill0x2A@users.noreply.github.com>
2023-08-08 12:55:14 +0700

    google protobuf construction with elliptic signing

--- src/app/api/sign-ledger-tx/route.ts
+++ src/app/api/sign-ledger-tx/route.ts
@@ -1,75 +1,79 @@
 import { NextResponse } from 'next/server.js';
 import { LedgerSignInput } from '@/app/types.js';
 import base64url from 'base64url';
-import sha256 from 'js-sha256';
-import secp256r1 from 'secp256r1';
+import { ec as EC } from 'elliptic';
+import { createHash } from 'crypto';
 import '../../lib/protocol_pb.js';
-import protobuf from 'protobufjs';
 
-// https://developers.ledger.com/docs/swap/howto/providers-endpoints/#protobuf-message-payload
-const PROTOBUF_SCHEMA = `
-package ledger_swap;
-syntax = "proto3";
-message NewTransactionResponse {
-  string    payin_address = 1;
-  string    payin_extra_id = 2;
-  string    refund_address = 3;
-  string    refund_extra_id = 4;
-  string    payout_address = 5;
-  string    payout_extra_id = 6;
-  string    currency_from = 7;
-  string    currency_to = 8;
-  bytes     amount_to_provider = 9;
-  bytes     amount_to_wallet = 10;
-  string    device_transaction_id = 11;
-}
-`;
- 
-const generateProtobufPayload = (input: LedgerSignInput): Buffer => {
-  const { root } = protobuf.parse(PROTOBUF_SCHEMA);
-  const NewTransactionResponse = root.lookupType('ledger_swap.NewTransactionResponse');
+// https://github.com/LedgerHQ/platform-app-test-exchange/blob/main/src/utils/numberToBigEndianBuffer.ts
+// x modified to be BigInt
+const numberToBigEndianBuffer = (x: BigInt) => {
+  var hex = x.toString(16);
+  return Uint8Array.from(
+    Buffer.from(hex.padStart(hex.length + (hex.length % 2), "0"), "hex")
+  );
+};
 
-  const proto = NewTransactionResponse.encode(input).finish();
-
-  return Buffer.from(proto)
-}
-
-// Based on:
 // https://github.com/LedgerHQ/platform-app-test-exchange/blob/main/src/getData/index.ts
-const generatePayloadAndSignature = (
-  input: LedgerSignInput
-) => {
+// Based on above, modified to use proto.ledger_swap.NewTransactionResponse (not included in example repo)
+const generatePayload = (input: LedgerSignInput): Buffer => {
+  const tr = new proto.ledger_swap.NewTransactionResponse();
+
+  tr.setPayinAddress(input.depositAddress);
+  input.depositMemo && tr.setPayinExtraId(input.depositMemo);
+  input.refundAddress && tr.setRefundAddress(input.refundAddress);
+  input.refundMemo && tr.setRefundExtraId(input.refundMemo);
+  tr.setPayoutAddress(input.settleAddress);
+  input.settleMemo && tr.setPayoutExtraId(input.settleMemo);
+  tr.setCurrencyFrom(input.depositMethodId);
+  tr.setCurrencyTo(input.settleMethodId);
+  tr.setAmountToProvider(numberToBigEndianBuffer(BigInt(input.depositAmount)));
+  tr.setAmountToWallet(numberToBigEndianBuffer(BigInt(input.settleAmount)));
+  tr.setDeviceTransactionId(input.deviceTransactionId);
+
+  // Serialize protobuf, return as buffer (modified from example to be compatible with Elliptic lib)
+  return Buffer.from(tr.serializeBinary());
+}
+
+const signPayloadElliptic = (
+  payload: Buffer,
+): Buffer => {
   const ledgerPrivateKey = process.env.LEDGER_PRIVATE_KEY;
 
   if (!ledgerPrivateKey) {
-    throw new Error("Ledger private key not configured");
+    throw new Error('LEDGER_PRIVATE_KEY should be set as a 64 characters long hex string (32 bytes) in .env.local');
   }
 
-  // Example constructs this from a Uint8Array, both converted to Buffer so equivalent
-  const TEST_PRIVATE_KEY = Buffer.from(ledgerPrivateKey, "hex");
+  const ec = new EC('secp256k1');
 
-  // Replaced with protobuf payload
-  const payload = generateProtobufPayload(input);
+  // privateKey should be a 64 characters long hex string (32 bytes)
+  const key = ec.keyFromPrivate(ledgerPrivateKey, 'hex');
 
-  const base64Payload = Buffer.from(base64url(payload));
+  // Hash the payload using SHA-256
+  const hash = createHash('sha256');
+  hash.update(payload);
+  const hashedPayload = hash.digest();
 
-  const message = Buffer.concat([Buffer.from("."), base64Payload]);
-  const digest = Buffer.from(sha256.sha256.array(message));
+  const signature = key.sign(hashedPayload);
 
-  const signature = Buffer.from(
-    secp256r1.sign(digest, TEST_PRIVATE_KEY).signature
-  );
+  // Ensure 'r' and 's' are 32 bytes each, pad if necessary
+  const r = Buffer.from(signature.r.toString(16).padStart(64, '0'), 'hex');
+  const s = Buffer.from(signature.s.toString(16).padStart(64, '0'), 'hex');
 
-  return { binaryPayload: base64Payload, signature };
+  // Concatenate r and s values
+  const r_s_signature = Buffer.concat([r, s]);
+
+  return r_s_signature;
 };
 
 export async function POST(req: any) {
   const { input }: { input: LedgerSignInput } = await req.json();
 
-  const { binaryPayload, signature } = generatePayloadAndSignature(input);
+  const payload = generatePayload(input);
+  const signature = signPayloadElliptic(payload);
 
   // Return payload and signature as base64url encoded strings
   return NextResponse.json(
-    { payload: base64url(binaryPayload), signature: base64url(signature) },
+    { payload: base64url(payload), signature: base64url(signature) },
   );
 }
