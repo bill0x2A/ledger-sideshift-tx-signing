77c2116e72930f7036dc654d35e0a70b7eeb7801
bill0x2a <8884279+bill0x2A@users.noreply.github.com>
2023-10-02 15:47:24 +0300

    correctly generate signature apdus

--- src/app/util/helpers.ts
+++ src/app/util/helpers.ts
@@ -8,16 +8,38 @@ const FEE_BYTES = "020da0";
 const SIG_DELIM_BYTES = "022100";
 
 const PROCESS_TX_CMD_PREFIX="e0060000b4b0";
-const CHECK_TX_SIG_CMD_PREFIX = "e0070000483046";
+const CHECK_TX_SIG_CMD_PREFIX = "e0070000";
+
+const DER_INT_IND = "02";
+const DER_SEQ_IND = "30";
 
 const generatePayloadAPDU = (serializedPayload: string): string => {
-    return `${PROCESS_TX_CMD_PREFIX}${serializedPayload}${FEE_BYTES}`;
+    return PROCESS_TX_CMD_PREFIX + serializedPayload + FEE_BYTES;
 };
 
 const generateSignatureCheckAPDU = (signature: string): string => {
-    const [first32Bytes, last32Bytes] = [signature.slice(0,32), signature.slice(32)];
-    
-    return `${CHECK_TX_SIG_CMD_PREFIX}${SIG_DELIM_BYTES}${first32Bytes}${SIG_DELIM_BYTES}${last32Bytes}`;
+    if (signature.length !== 128) {
+        throw new Error('Invalid signature length');
+    }
+
+    // Extract R and S components from the signature
+    const r = signature.slice(0, 64);
+    const s = signature.slice(64, 128);
+
+    // Ensure R and S are interpreted as positive values
+    const rVal = (parseInt(r.substring(0, 2), 16) >= 0x80) ? `00${r}` : r;
+    const sVal = (parseInt(s.substring(0, 2), 16) >= 0x80) ? `00${s}` : s;
+
+    // Calculate lengths
+    const rLen = (rVal.length / 2).toString(16).padStart(2, '0');
+    const sLen = (sVal.length / 2).toString(16).padStart(2, '0');
+
+    const totalDataLength = ((rVal.length + sVal.length) / 2 + 6).toString(16).padStart(2, '0');
+    const totalDerLength = ((rVal.length + sVal.length) / 2 + 4).toString(16).padStart(2, '0');
+
+    // Construct the APDU
+    return CHECK_TX_SIG_CMD_PREFIX + totalDataLength + DER_SEQ_IND + totalDerLength + DER_INT_IND + rLen + rVal + DER_INT_IND + sLen + sVal;
 };
+
 
 export { numberToBigEndianBuffer, generatePayloadAPDU, generateSignatureCheckAPDU };
